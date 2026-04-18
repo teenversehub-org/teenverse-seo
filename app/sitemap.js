cc8b1d2ed025390646503c8882688739f6b59915
kashifkhan <99235937+9511028284@users.noreply.github.com>
2026-04-17 22:39:52 -0700

    Update sitemap URLs to use 'www' prefix

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,27 +1,27 @@
 export default function sitemap() {
   return [
     {
-      url: 'https://teenversehub.in',
+      url: 'https://www.teenversehub.in',
       lastModified: new Date(),
     },
     {
-      url: 'https://teenversehub.in/hire-teen-freelancers',
+      url: 'https://www.teenversehub.in/hire-teen-freelancers',
       lastModified: new Date(),
     },
      {
-      url: 'https://teenversehub.in/freelance-jobs-for-teens',
+      url: 'https://www.teenversehub.in/freelance-jobs-for-teens',
       lastModified: new Date(),
     },
     {
-      url: 'https://teenversehub.in/about',
+      url: 'https://www.teenversehub.in/about',
       lastModified: new Date(),
     },
     {
-      url: 'https://teenversehub.in/safety',
+      url: 'https://www.teenversehub.in/safety',
       lastModified: new Date(),
     },
     {
-      url: 'https://teenversehub.in/faq',
+      url: 'https://www.teenversehub.in/faq',
       lastModified: new Date(),
     },
   ];
